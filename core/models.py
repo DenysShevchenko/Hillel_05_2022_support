97f5a546ae5934329ae5e470f359776fbe2d6b3c
DenysShevchenko <shevado@gmail.com>
2022-07-27 16:55:52 +0300

    test 1

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.db import models
 
 
 class TimeStampMixin(models.Model):
